2ef6a791949e5732dbfb6d347b170dee131dbac7
James <jamiesmith3979@gmail.com>
2025-02-23 00:06:28 +0000

    Added basic GUI for registration, will need styling but will be a foundation to begin the functionality.

--- src/frontend/src/routes/home/+page.svelte
+++
@@ -1,1 +0,0 @@
-Testing home page! Testing watch!
---
+++ src/frontend/src/routes/register/+page.svelte
@@ -0,0 +1,24 @@
+<style>
+
+</style>
+
+<main>
+
+    <h1>Enter your details:</h1>
+    <form action="/submit" method="POST">
+        <div>
+            <label for="username">Name:</label>
+            <input type="text" id="name" name="name" required />
+        </div>
+
+        <div>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required />
+        </div>
+
+        <div>
+            <button type="submit">Submit</button>
+        </div>
+    </form>
+</main>
+
