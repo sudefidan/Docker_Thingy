d45dee2c87609ed1e276c945ce99c277faddad88
s2-fidan <sude2.fidan@live.uwe.ac.uk>
2025-03-03 12:36:47 +0000

    #close UNIHUB-14-homepage-gui

--- src/frontend/src/routes/home/+page.svelte
+++ src/frontend/src/routes/home/+page.svelte
@@ -3,7 +3,7 @@
 
 <main>
 	<div>
-		<h1 class="mb-4 text-4xl font-bold">Welcome to the Homepage</h1>
-		<p class="text-lg">This is the homepage with a blue background.</p>
+		<h1 class="mb-4 text-4xl font-bold">Hub Page</h1>
+		<p class="text-lg">This is the hub page with a blue background.</p>
 	</div>
 </main>
