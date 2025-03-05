7914dc2157d16b0cf13bec29afb27a40f8bc446c
s2-fidan <sude2.fidan@live.uwe.ac.uk>
2025-03-05 12:56:56 +0000

    register gui password show&hide, rerouting to home after registration

--- src/frontend/src/routes/register/+page.svelte
+++ src/frontend/src/routes/register/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	import { goto } from '$app/navigation';
+
+	let showPassword = false;
 
 	let registerForm: HTMLFormElement;
 
@@ -22,7 +25,7 @@
 						// Not getting expected SvelteKit response as Django is responding, not SvelteKit. We either send a message or an error.
 						const customResult = result as CustomResult;
 						if (customResult?.message) {
-							alert(customResult.message);
+							goto('/home');
 							registerForm.reset();
 						} else if (customResult?.error) {
 							alert(customResult.error);
@@ -107,16 +110,60 @@
 						<label for="password" class="label">
 							<span class="label-text">Password</span>
 						</label>
+						<div class="relative">
 						<input
-							type="password"
+							type={showPassword ? 'text' : 'password'}
 							name="password"
-							class="input input-bordered validator focus:outline-primary bg-secondary text-base-100 w-full rounded-md focus:outline-2 focus:-outline-offset-2"
+							class="input input-bordered validator focus:outline-primary bg-secondary text-base-100 w-full rounded-md focus:outline-2 focus:-outline-offset-2 pr-10"
 							required
 							minlength="8"
 							autocomplete="new-password"
 							placeholder="Enter your password"
 						/>
+						<button
+								type="button"
+								class="absolute inset-y-0 right-0 flex items-center pb-6 pr-3 text-sm leading-5"
+								on:click={() => (showPassword = !showPassword)}
+							>
+								{#if showPassword}
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke-width="1.5"
+										stroke="currentColor"
+										class="size-5"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+										/>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+										/>
+									</svg>
+								{:else}
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke-width="1.5"
+										stroke="currentColor"
+										class="size-5"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
+										/>
+									</svg>
+								{/if}
+								</button>
 						<p class="validator-hint">Must be more than 8 characters!</p>
+						</div>
 					</div>
 				</div>
 				<div class="form-control mb-4 flex justify-center">
