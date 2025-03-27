2957e6d068c409f1c32bc15b358d118886ed46b5
s2-fidan <sude2.fidan@live.uwe.ac.uk>
2025-03-27 18:56:20 +0000

    unnecessary community arrays deleted & community leader adding is changed to modify with user_id for user selection instead of a text & fetch community leaders list

--- src/backend/app/views.py
+++ src/backend/app/views.py
@@ -74,7 +74,7 @@ class login_user(APIView):
         password = request.data.get('password')
 
         user = authenticate(username=username, password=password)
-        if user: 
+        if user:
             refresh = RefreshToken.for_user(user)
             return Response({
                 'access': str(refresh.access_token),
@@ -103,46 +103,46 @@ class logout_user(APIView):
             return Response({'message': 'Logout successful'}, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({'error': 'Invalid Token'}, status=status.HTTP_400_BAD_REQUEST)
-    
-    
+
+
 class user_profile_view(APIView):
     permission_classes = [IsAuthenticated]
-    
+
     def get(self, request, user_id):
-        
+
         user = get_object_or_404(User, pk=user_id)
-        
+
         return Response({
             "username": user.username,
             "forename": user.first_name,
             "surname": user.last_name,
             "email": user.email
         })
-        
+
 '''class upload_profile_picture(APIView):
     permission_classes = [IsAuthenticated]
-    
+
     def post(self, request):
         user = request.user
-        
+
         if "profile_picture" not in request.FILES:
             return Response({"error": "No file uploaded"}, status=status.HTTP_400_BAD_REQUEST)
-        
+
         file = request.FILES["profile_picture"]
-        
+
         if isinstance(file, InMemoryUploadedFile):
             user.profile_picture = file.read()
             user.save()
             return Response({"error": "Profile picture uploaded successfully"}, status=status.HTTP_200_OK)
-        
+
         return Response({"error": "Invalid file"}, status=status.HTTP_400_BAD_REQUEST)
-    
+
 class GetProfilePicture(APIView):
-    permission_classes = [IsAuthenticated]  
+    permission_classes = [IsAuthenticated]
 
     def get(self, request):
         # Fetches users profile picture and returns it as a b64 string
-        
+
         user = request.user
         if user.profile_picture:
             base64_image = base64.b64encode(user.profile_picture).decode("utf-8")
@@ -150,7 +150,7 @@ class GetProfilePicture(APIView):
 
         return Response({"profile_picture": None}, status=status.HTTP_200_OK)
 '''
-    
+
 
 class create_community(APIView):
     permission_classes = [AllowAny]
@@ -165,7 +165,7 @@ class create_community(APIView):
             owner_id = request.user.id
         else:
             return Response({"error": "User must be logged in"}, status=400)
-          
+
         if Community.objects.filter(name__iexact=name).exists():
             return Response({"error": "This community already exists."}, status=400)
 
@@ -181,7 +181,7 @@ class create_community(APIView):
             user = get_object_or_404(User, id=leader_id)
             CommunityLeader.objects.create(community=community, user=user)
             Subscribed.objects.create(community=community, user=user)
-            
+
         # auto subscribe the current userid that is logged to the community
         Subscribed.objects.create(community=community, user_id=owner_id)
 
@@ -189,10 +189,10 @@ class create_community(APIView):
             "community_id": community.community_id,
             "message": "Community created successfully and selected leaders assigned"
         })
-    
+
 class SubscribedCommunities(APIView):
     # the user must be logged in otherwise it will not work
-    permission_classes = [IsAuthenticated] 
+    permission_classes = [IsAuthenticated]
 
     def get(self, request):
         user = request.user
@@ -200,7 +200,7 @@ class SubscribedCommunities(APIView):
         subscriptions = Subscribed.objects.filter(user=user)
         # Get the list of communities that have been subscribed too
         communities = [sub.community for sub in subscriptions]
-        
+
         # Serialize the response
         community_data = [
             {
@@ -209,7 +209,7 @@ class SubscribedCommunities(APIView):
                 "description": community.description,
                 "category": community.category,
                 "owner_id": community.owner_id
-            } 
+            }
             for community in communities
         ]
 
@@ -226,15 +226,15 @@ def get_users(request):
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def get_posts(request):
-    posts = Post.objects.all().order_by('-date') 
+    posts = Post.objects.all().order_by('-date')
     posts_data = [
         {
             'id': post.post_id,
             'title': post.title,
             'content': post.content,
-            'date': post.date.isoformat(),  
-            'user_id': post.user.id,  
-            'username': post.user.username,  
+            'date': post.date.isoformat(),
+            'user_id': post.user.id,
+            'username': post.user.username,
         }
         for post in posts
     ]
@@ -248,8 +248,8 @@ def create_post(request):
     if request.method == 'POST':
         data = request.data
 
-        title = data.get('title')  
-        content = data.get('content')  
+        title = data.get('title')
+        content = data.get('content')
         date = data.get('date')
 
         if not title or not content:
@@ -260,7 +260,7 @@ def create_post(request):
             title=title,
             content=content,
             date = date,
-            user=request.user  
+            user=request.user
         )
 
         return Response({
@@ -270,7 +270,7 @@ def create_post(request):
             'user_id': post.user.id,
             'username': post.user.username
         }, status=201)
-    
+
 class CreateCommunity(APIView):
     permission_classes = [AllowAny]
 
@@ -313,7 +313,7 @@ def fetch_communities(request):
     if request.method == "GET":
         communities = Community.objects.all().values()
         return JsonResponse(list(communities), safe=False)
-    
+
     return JsonResponse({"error": "Invalid request"}, status=400)
 
 
@@ -335,49 +335,6 @@ def join_community(request, community_id):
 
     return Response({"message": "Successfully joined the community!"})
 
-
-@api_view(["GET"])
-@permission_classes([IsAuthenticated])
-def fetch_your_communities(request):
-    """Fetch all communities a user has joined."""
-    user = request.user
-    communities = Community.objects.filter(community_id__in=
-        Subscribed.objects.filter(user_id=user.id).values_list("community_id", flat=True)
-    )
-
-    community_data = [
-        {
-            'community_id': community.community_id,
-            'name': community.name,
-            'description': community.description,
-            'category': community.category,
-            'is_owner': community.owner_id == user.id
-        }
-        for community in communities
-    ]
-    
-    return Response(community_data, status=200)
-
-@api_view(["GET"])
-@permission_classes([IsAuthenticated])
-def fetch_owned_communities(request):
-    """Fetch all communities a user owns."""
-    user = request.user
-    communities = Community.objects.filter(owner=user)
-
-    community_data = [
-        {
-            'community_id': community.community_id,
-            'name': community.name,
-            'description': community.description,
-            'category': community.category,
-            'is_owner': True  # Since these are owned communities
-        }
-        for community in communities
-    ]
-
-    return Response(community_data, status=200)
-
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
 def leave_community(request):
@@ -388,10 +345,10 @@ def leave_community(request):
             membership = Subscribed.objects.get(user=user, community_id=community_id)
             membership.delete()
             return Response({"error": "Successfully left the community"})
-        
+
         finally:
             return Response({"error": "You are not a member of this community"}, status=400)
-        
+
         # except Subscribed.DoesNotExsist:
         #     return Response({"error": "You are not a member of this community"}, status=400)
 
@@ -629,13 +586,13 @@ def delete_community_leader(request, community_id, leader_id):
 
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
-def add_community_leader(request, community_id, username):
+def add_community_leader(request, community_id, user_id):
     """
     Adds a user as a leader to a community.
     """
     try:
         community = get_object_or_404(Community, community_id=community_id)
-        user_to_add = get_object_or_404(User, username=username)
+        user_to_add = get_object_or_404(User, id=user_id)
     except ValueError:
         return Response({"error": "Invalid community or user ID format."}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -654,7 +611,7 @@ def add_community_leader(request, community_id, username):
             # Subscribe the user to the community only if they are not already subscribed
             Subscribed.objects.create(community=community, user=user_to_add)
         message_leader = f"You are now a leader of {community.name}."
-        create_notification(user_to_add.id, message_leader) 
+        create_notification(user_to_add.id, message_leader)
         message_owner = f"User {user_to_add.username} is now a leader of {community.name}."
         create_notification(request.user.id, message_owner)
         return Response({"message": f"User {user_to_add.username} is now a leader of {community.name}."}, status=status.HTTP_201_CREATED)
--- src/backend/proj/urls.py
+++ src/backend/proj/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, re_path, include
 from app import views
 from app.views import login_user, logout_user, user_profile_view
 from rest_framework import routers, serializers, viewsets
-from app.views import get_users, join_community, fetch_communities, fetch_your_communities, leave_community, fetch_owned_communities, update_community_name, update_community_description, update_community_category, delete_community, get_notifications, delete_notification, get_community_leaders, delete_community_leader, add_community_leader
+from app.views import get_users, join_community, fetch_communities, leave_community, update_community_name, update_community_description, update_community_category, delete_community, get_notifications, delete_notification, get_community_leaders, delete_community_leader, add_community_leader
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -28,10 +28,8 @@ urlpatterns = [
     path('api/create_posts/', views.create_post, name='create_post'),
     path("api/communities/", fetch_communities, name="fetch_communities"),
     path("api/join_community/<int:community_id>/", join_community, name="join_community"),
-    path('api/your_communities/', fetch_your_communities, name="fetch_your_communities"),
     path("api/leave_community/", leave_community, name="leave_community"),
     path('api/subscribed_communities/', views.SubscribedCommunities.as_view(), name='subscribed_communities'),
-    path('api/fetch_owned_communities/', fetch_owned_communities, name="fetch_owned_communities"),
     path('api/communities/update_community_name/', update_community_name, name="update_community_name"),
     path('api/communities/update_community_description/', update_community_description, name="update_community_description"),
     path('api/communities/update_community_category/', update_community_category, name="update_community_category"),
@@ -40,5 +38,5 @@ urlpatterns = [
     path('api/notifications/delete/<int:notification_id>/', delete_notification, name="delete_notification"),
     path('api/community/get_leaders/<int:community_id>/', get_community_leaders, name="get_community_leaders"),
     path('api/community/<int:community_id>/leaders/<int:leader_id>/delete/', delete_community_leader, name="delete_community_leaders"),
-    path('api/community/<int:community_id>/leaders/<str:username>/add/', add_community_leader, name='add_community_leader'),
+    path('api/community/<int:community_id>/leaders/<int:user_id>/add/', add_community_leader, name='add_community_leader'),
 ]
