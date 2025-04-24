79c3ab2d0a51b2c4b28e0a91a0706f93ecc7dc8c
s2-fidan <sude2.fidan@live.uwe.ac.uk>
2025-04-24 17:28:55 +0100

    done

---
+++ src/frontend/src/assets/SettingsIcon.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let size = 32; // Default size
+</script>
+
+<svg
+	fill="currentColor"
+	width={size}
+	height={size}
+	viewBox="0 0 210 256.00"
+	id="Flat"
+	xmlns="http://www.w3.org/2000/svg"
+	stroke="currentColor"
+	stroke-width="4.864"
+	><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
+		id="SVGRepo_tracerCarrier"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	></g><g id="SVGRepo_iconCarrier">
+		<path
+			d="M128,28A100,100,0,1,0,228,128,100.11332,100.11332,0,0,0,128,28Zm0,192a92,92,0,1,1,92-92A92.10416,92.10416,0,0,1,128,220Zm8-92a8,8,0,1,1-8-8A8.00917,8.00917,0,0,1,136,128Zm48,0a8,8,0,1,1-8-8A8.00917,8.00917,0,0,1,184,128Zm-96,0a8,8,0,1,1-8-8A8.00917,8.00917,0,0,1,88,128Z"
+		></path>
+	</g></svg
+>
